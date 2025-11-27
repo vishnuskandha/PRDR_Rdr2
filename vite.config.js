94387abb1d7ef683d4553b9ee67c912062778ede
Vishnu Skandha <vishnuskandha@example.com>
2025-11-27 19:35:55 +0530

    Fix: Update base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/rdr2-prdr-converter/', // Update this to match your GitHub repository name
+  base: '/PRDR_Rdr2/', // Update this to match your GitHub repository name
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
